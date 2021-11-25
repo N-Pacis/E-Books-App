9db926e856fb5c9b4ed1a9cebad8ae438f40a8e5
Nkubito Pacis <pacisnkubito@gmail.com>
2021-11-25 13:44:26 +0200

    [Feat] Added continue reading component

--- src/components/ExploreBooksComponent.js
+++ src/components/ExploreBooksComponent.js
@@ -1,120 +1,32 @@
-// import React, { useState } from 'react';
-// import { SafeAreaView,Image, StyleSheet, Text, View } from 'react-native';
-// import { FlatList, ScrollView } from 'react-native-gesture-handler';
-
-// export default function ExploreBooksComponent() {
-//   const [books,setBook] = useState([
-//         {
-//             "id":1,
-//             "title":"There There",
-//             "author":"Tummy Orange",
-//             "cover":"https://images.penguinrandomhouse.com/cover/9780525436140"    
-//         },
-//         {
-//             "id":2,
-//             "title":"The great gatsby",
-//             "author":"Scott Fitzgerald",
-//             "cover":"https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg"    
-//         },
-//         {
-//             "id":3,
-//             "title":"Ugly Love",
-//             "author":"Colleen Hoover",
-//             "cover":"https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781476753188/ugly-love-9781476753188_lg.jpg"    
-//         },
-//         {
-//             "id":4,
-//             "title":"Room",
-//             "author":"Emma Donoghue",
-//             "cover":"https://evoke.ie/wp-content/uploads/2020/01/51AlO2Ak0VL.jpg"    
-//         }
-//   ])
-//   const Book = ({cover})=>(
-//     <View style={styles.bookItem}>
-//         <Image source={{ uri: cover }} style={styles.bookItem__image}/>
-//     </View>
-//   );
-//   const renderBook = ({book})=><Book cover={book.cover}/>
-//   return (
-//     <SafeAreaView style={styles.container}>
-//         <FlatList data={books} renderItem={renderBook} />            
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         paddingHorizontal:15,
-//         paddingTop: 50
-//     },
-    
-//     viewBooks:{
-//         flexGrow:1,
-//         height:0,
-//         display:'flex',
-//         padding:'2%',
-//         width:'100%',
-//     },
-//     bookItem:{
-//         marginHorizontal:10,
-//         flexGrow:0,
-//         width:'40%',
-//         borderRadius:5,
-//         marginVertical:5,
-//         height:280,
-//     },
-//     bookItem__image:{
-//         borderRadius:5,
-//         width:'98%',
-//         marginTop:'1.3%',
-//         height:'80%'
-//     },
-//     bookItem__title:{
-//         fontSize:18,
-//         fontFamily:'sans-serif',
-//         fontWeight:"bold",
-//         color:'#000',
-//         marginBottom:'-2%',
-//     },
-//     bookItem__author:{
-//         fontSize:15,
-//         fontFamily:'sans-serif',
-//         color:'#8f8b8b'
-//     }
-// });
-
 import React from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,Image } from 'react-native';
-// import { SafeAreaView,Image, StyleSheet, Text, View } from 'react-native';
-
+import * as Progress from 'react-native-progress';
 
 const Books = [
+  {
+    "id":1,
+    "title":"Room",
+    "author":"Emma Donoghue",
+    "cover":"https://evoke.ie/wp-content/uploads/2020/01/51AlO2Ak0VL.jpg"    
+  },
     {
-        "id":1,
-        "title":"There There",
-        "author":"Tummy Orange",
-        "cover":"https://images.penguinrandomhouse.com/cover/9780525436140"    
+      "id":2,
+      "title":"Ugly Love",
+      "author":"Colleen Hoover",
+      "cover":"https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781476753188/ugly-love-9781476753188_lg.jpg"    
     },
     {
-        "id":2,
+        "id":3,
         "title":"The great gatsby",
         "author":"Scott Fitzgerald",
         "cover":"https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg"    
     },
     {
-        "id":3,
-        "title":"Ugly Love",
-        "author":"Colleen Hoover",
-        "cover":"https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781476753188/ugly-love-9781476753188_lg.jpg"    
-    },
-    {
-        "id":4,
-        "title":"Room",
-        "author":"Emma Donoghue",
-        "cover":"https://evoke.ie/wp-content/uploads/2020/01/51AlO2Ak0VL.jpg"    
-    },
+      "id":4,
+      "title":"There There",
+      "author":"Tummy Orange",
+      "cover":"https://images.penguinrandomhouse.com/cover/9780525436140"    
+  },
     {
         "id":5,
         "title":"Dark Rise",
@@ -133,7 +45,37 @@ const Books = [
         "author":"Catherine Taylor",
         "cover":"https://images-na.ssl-images-amazon.com/images/I/61OEvJKs3rL.jpg"
     }
-    ];
+];
+const ContinueReading = [
+    {
+      "id":1,
+      "title":"Dune Messiah",
+      "author":"Frank Herbert",
+      "cover":"https://images4.penguinrandomhouse.com/cover/9780593201732",
+      "progress":0.8    
+  },
+  {
+      "id":2,
+      "title":"Baby's First Felony",
+      "author":"John Straley",
+      "cover":"https://images4.penguinrandomhouse.com/cover/9781641290630",
+      "progress":0.3    
+  },
+  {
+    "id":3,
+    "title":"Lady Audley's Secret",
+    "author":"Mary Elizabeth Braddon",
+    "cover":"https://images4.penguinrandomhouse.com/cover/9781984854193",
+    "progress":0.1
+  },
+  {
+      "id":4,
+      "title":"Ugly Love",
+      "author":"Colleen Hoover",
+      "cover":"https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781476753188/ugly-love-9781476753188_lg.jpg",
+      "progress":0.7    
+  }
+]
  
 const Book = ({ cover,title,author }) => (
   <View style={styles.bookItem}>
@@ -143,73 +85,38 @@ const Book = ({ cover,title,author }) => (
   </View>
 );
 
+const ReadingBook = ({ cover,title,author,progress }) => (
+  <View style={styles.ReadingBookItem}>
+    <Image source={{ uri: cover }} style={styles.ReadingBookItem__image}/>
+    <Text style={styles.ReadingBookItem__title}>{title}</Text>
+    <Text style={styles.ReadingBookItem__author}>{author}</Text>
+    <Progress.Bar progress={progress} width={170} height={7} borderRadius={5} borderColor={'transparent'} unfilledColor={'#e8eaed'} />
+  </View>
+);
+
 const App = () => {
-  const renderItem = ({ item }) => <Book cover={item.cover} title={item.title} author={item.author}/>;
+  const renderBooks = ({ item }) => <Book cover={item.cover} title={item.title} author={item.author}/>;
+  const renderReadingBooks = ({ item }) => <ReadingBook cover={item.cover} title={item.title} author={item.author} progress={item.progress}/>;
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList data={Books} horizontal renderItem={renderItem} keyExtractor={item => item.id} />
+      <StatusBar hidden/>
+      <FlatList style={styles.Books}data={Books} horizontal renderItem={renderBooks} keyExtractor={item => item.id} />
+      <Text style={styles.continueReading}>Continue Reading</Text>
+      <FlatList data={ContinueReading} horizontal renderItem={renderReadingBooks} keyExtractor={item=>item.id}/>
     </SafeAreaView>
   );
 };
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         paddingHorizontal:15,
-//         paddingTop: 50
-//     },
-    
-//     viewBooks:{
-//         flexGrow:1,
-//         height:0,
-//         display:'flex',
-//         padding:'2%',
-//         width:'100%',
-//     },
-//     bookItem:{
-//         marginHorizontal:10,
-//         flexGrow:0,
-//         width:'40%',
-//         borderRadius:5,
-//         marginVertical:5,
-//         height:280,
-//     },
-//     bookItem__image:{
-//         borderRadius:5,
-//         width:'98%',
-//         marginTop:'1.3%',
-//         height:'80%'
-//     },
-//     bookItem__title:{
-//         fontSize:18,
-//         fontFamily:'sans-serif',
-//         fontWeight:"bold",
-//         color:'#000',
-//         marginBottom:'-2%',
-//     },
-//     bookItem__author:{
-//         fontSize:15,
-//         fontFamily:'sans-serif',
-//         color:'#8f8b8b'
-//     }
-// });
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal:5,
     marginTop: StatusBar.currentHeight || 0
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
+  Books:{
+    height:140
+  }, 
   bookItem: {
     marginHorizontal:10,
     width:175,
@@ -240,6 +147,45 @@ const styles = StyleSheet.create({
     fontWeight:"bold",
     opacity:0.8,
     color:'#9a9c9c'
+  },
+  continueReading:{
+    fontSize:18,
+    fontFamily:'sans-serif',
+    marginLeft:12,
+    marginBottom:4,
+    fontWeight:'bold'
+  },
+  ReadingBookItem: {
+    marginHorizontal:10,
+    width:175,
+    borderRadius:5,
+    marginVertical:5,
+    height:280,
+  },
+  ReadingBookItem__image: {
+    width:'100%',
+    borderRadius:5,
+    height:'80%'
+  },
+  ReadingBookItem__title:{
+    marginTop:3,
+    fontSize:18,
+    fontFamily:'sans-serif',
+    opacity:0.9,
+    marginLeft:3,
+    fontFamily:'sans-serif',
+    fontWeight:"bold",
+    color:'#161717',
+    marginBottom:2
+  },
+  ReadingBookItem__author:{
+    fontSize:15,
+    marginLeft:3,
+    fontFamily:'sans-serif',
+    fontWeight:"bold",
+    opacity:0.8,
+    color:'#9a9c9c',
+    marginBottom:10
   }
 });
 
